dcbec324e16f62065da75ff40018a0b5b100ceba
Chikavi1 <chikavi@hotmail.com>
2022-12-21 18:00:00 -0600

    intento de anuncios

--- src/app/pages/finish/finish.page.ts
+++ src/app/pages/finish/finish.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdMob,RewardAdOptions,RewardAdPluginEvents,AdMobRewardItem } from '@capacitor-community/admob';
 
 @Component({
   selector: 'app-finish',
@@ -9,12 +10,44 @@ export class FinishPage implements OnInit {
   recommend;
   slide;
 
-  constructor() { }
+  constructor() {
+    this.initialize();
+   }
 
   ngOnInit() {
+  }
+
+  async initialize(){
+    const { status} = await AdMob.trackingAuthorizationStatus();
+    console.log(status);
+
+    AdMob.initialize({
+      requestTrackingAuthorization: true,
+      testingDevices: [''],
+      initializeForTesting: true,
+    })
+
   }
 
   setRecommend(s){
 
   }
+
+  async showRewardVideo(){
+    AdMob.addListener(
+      RewardAdPluginEvents.Rewarded,
+      (reward: AdMobRewardItem) => {
+        console.log('REWARD: ',reward);
+      }
+    )
+    const options: RewardAdOptions = {
+      adId: '9906704246',
+      isTesting: true
+    };
+
+    await AdMob.prepareRewardVideoAd(options);
+    await AdMob.showRewardVideoAd();
+  }
+
+
 }
